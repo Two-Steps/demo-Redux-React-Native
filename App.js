ccc7c1455c8140dd4e72bd698c4c4529019583cc
Phạm Xuân Tùng <tungphamepu@gmail.com>
2021-05-24 08:57:16 +0700

    commit

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MainStackNavigator from './src/navigation/AppNavigator'
+import { Provider as StoreProvider } from 'react-redux'
+import store from './src/store'
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <StoreProvider store={store}>
+      <MainStackNavigator />
+    </StoreProvider>
   );
 }
 
@@ -15,7 +18,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
